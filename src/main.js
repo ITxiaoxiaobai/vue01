84798e09f7d881b072100e1b61d7d83d89a1bdd1
黄健 <1342248895@qq.com>
2022-04-06 16:31:18 +0800

    新增axios.config.js配置文件 新增vue.config.js配置文件 注册element组件库 注册vant组件库

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import element from 'element-ui'
+import vant from 'vant'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(element)
+Vue.use(vant)
 new Vue({
   router,
   store,
